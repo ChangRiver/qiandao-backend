2af9de4473f914d5db37a3cfb1fa7791a4fb25f2
unknown <819669045@qq.com>
2020-09-29 16:58:06 +0800

    创建服务

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-var express = require('express');
+var express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    errorhandler = require('errorhandler'),
+    morgan = require('morgan'),
+    mongoose = require('mongoose');
+
+
+var isProduction = process.env.NODE_ENV === 'production';
+
 var app = express();
 
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(require('method-override')());
+app.use(express.static(__dirname + '/public'));
+
+if(!isProduction) {
+    app.use(errorhandler());
+}
+
+if(isProduction) {
+    mongoose.connect(process.env.MONGODB_URI);
+} else {
+    mongoose.connect('mongodb://localhost/qiandao');
+    mongoose.set('debug', true);
+}
+
+var server = app.listen(process.env.PORT || 3000, function(){
+    console.log('Listening on port ' + server.address().port);
+})
